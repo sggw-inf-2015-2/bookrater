06311102ec8b63931779b7906556bc843477b848
Adrian Borucki <gentoolx@gmail.com>
2018-06-02 14:39:05 +0200

    Reformatting

--- bookrater.py
+++ bookrater.py
@@ -12,14 +12,17 @@ from torch.autograd import Variable
 from flask_graphql import GraphQLView
 from flask import Flask
 
-class Query(gp.ObjectType):
 
-    predicted_ratings = gp.Field(gp.List(gp.Float),
-                                 users=gp.NonNull(gp.List(gp.Int)),
-                                 books=gp.NonNull(gp.List(gp.Int)))
+class GetRatings(gp.ObjectType):
+
+    predicted_ratings = gp.Field(
+        gp.List(gp.Float),
+        users=gp.NonNull(gp.List(gp.Int)),
+        books=gp.NonNull(gp.List(gp.Int)))
 
     def resolve_predicted_ratings(self, info, users, books):
-        data_in = pd.DataFrame.from_dict(OrderedDict([('userID', users), ('bookID', books)]))
+        data_in = pd.DataFrame.from_dict(
+            OrderedDict([('userID', users), ('bookID', books)]))
 
         data_tsr = torch.LongTensor(data_in.as_matrix())
         data_var = Variable(data_tsr, volatile=True)
@@ -36,7 +39,7 @@ class EmbeddingDot(nn.Module):
         self.b = nn.Embedding(n_books, n_factors)
         self.u.weight.data.uniform_(0, 0.05)
         self.b.weight.data.uniform_(0, 0.05)
-        
+
     def forward(self, cats, conts):
         users, books = cats[:, 0], cats[:, 1]
         u, b = self.u(users), self.b(books)
@@ -50,7 +53,9 @@ N_FACTORS = 50
 model = EmbeddingDot(n_users, n_books, N_FACTORS)
 model.load_state_dict(torch.load('bookweb-embed-dot.model'))
 
-schema = gp.Schema(query=Query)
+schema = gp.Schema(query=GetRatings)
 
 app = Flask(__name__)
-app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
+app.add_url_rule(
+    '/graphql',
+    view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
